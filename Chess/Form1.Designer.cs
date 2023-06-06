706ed57c585b4193557a8459b0544884876d25e2
cromeros <cromerosanchez@wsu.edu'clear>
2023-06-06 10:16:29 -0700

    Working on getting the chessboard working using a datagrid to do so. Need to make sure the cells are square and that they are in fact black and white based on the a chessboard. Got a good basic initializegrid() method going need to keep building on it.

--- Chess/Form1.Designer.cs
+++ Chess/Form1.Designer.cs
@@ -28,56 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            button2 = new Button();
+            menuPanel = new Panel();
             textBox1 = new TextBox();
+            multiplayerButton = new Button();
+            SinglePlayerButton = new Button();
+            chessBoard = new DataGridView();
+            menuPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)chessBoard).BeginInit();
             SuspendLayout();
             // 
-            // button1
+            // menuPanel
             // 
-            button1.Location = new Point(301, 128);
-            button1.Name = "button1";
-            button1.Size = new Size(165, 44);
-            button1.TabIndex = 0;
-            button1.Text = "Single Player";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(301, 178);
-            button2.Name = "button2";
-            button2.Size = new Size(165, 44);
-            button2.TabIndex = 1;
-            button2.Text = "Multiplayer";
-            button2.UseVisualStyleBackColor = true;
+            menuPanel.Controls.Add(textBox1);
+            menuPanel.Controls.Add(multiplayerButton);
+            menuPanel.Controls.Add(SinglePlayerButton);
+            menuPanel.Dock = DockStyle.Fill;
+            menuPanel.Location = new Point(0, 0);
+            menuPanel.Name = "menuPanel";
+            menuPanel.Size = new Size(800, 450);
+            menuPanel.TabIndex = 3;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(334, 99);
+            textBox1.Location = new Point(351, 164);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(100, 23);
-            textBox1.TabIndex = 2;
+            textBox1.TabIndex = 5;
             textBox1.Text = "Menu";
             textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // multiplayerButton
+            // 
+            multiplayerButton.Location = new Point(318, 243);
+            multiplayerButton.Name = "multiplayerButton";
+            multiplayerButton.Size = new Size(165, 44);
+            multiplayerButton.TabIndex = 4;
+            multiplayerButton.Text = "Multiplayer";
+            multiplayerButton.UseVisualStyleBackColor = true;
+            // 
+            // SinglePlayerButton
+            // 
+            SinglePlayerButton.AutoSize = true;
+            SinglePlayerButton.Location = new Point(318, 193);
+            SinglePlayerButton.Name = "SinglePlayerButton";
+            SinglePlayerButton.Size = new Size(165, 44);
+            SinglePlayerButton.TabIndex = 3;
+            SinglePlayerButton.Text = "Single Player";
+            SinglePlayerButton.UseVisualStyleBackColor = true;
+            SinglePlayerButton.Click += SinglePlayerButton_Click;
+            // 
+            // chessBoard
+            // 
+            chessBoard.AllowUserToAddRows = false;
+            chessBoard.AllowUserToDeleteRows = false;
+            chessBoard.AllowUserToResizeColumns = false;
+            chessBoard.AllowUserToResizeRows = false;
+            chessBoard.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            chessBoard.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            chessBoard.Location = new Point(175, 0);
+            chessBoard.Name = "chessBoard";
+            chessBoard.ReadOnly = true;
+            chessBoard.RowTemplate.Height = 25;
+            chessBoard.ScrollBars = ScrollBars.None;
+            chessBoard.Size = new Size(450, 450);
+            chessBoard.TabIndex = 4;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(textBox1);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            Controls.Add(chessBoard);
+            Controls.Add(menuPanel);
             Name = "Form1";
             Text = "Chess";
+            menuPanel.ResumeLayout(false);
+            menuPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)chessBoard).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
-        private Button button2;
+        private Panel menuPanel;
         private TextBox textBox1;
+        private Button multiplayerButton;
+        private Button SinglePlayerButton;
+        private DataGridView chessBoard;
     }
 }
--- Chess/Form1.cs
+++ Chess/Form1.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms;
+
 namespace Chess
 {
     public partial class Form1 : Form
@@ -5,6 +7,38 @@ namespace Chess
         public Form1()
         {
             InitializeComponent();
+            InitializeGrid();
+        }
+
+        private void SinglePlayerButton_Click(object sender, EventArgs e)
+        {
+            this.menuPanel.Visible = false;
+            this.chessBoard.Visible = true;
+        }
+
+        private void InitializeGrid()
+        {
+            string alphabet = "ABCDEFGH";
+            int numberOfRows = 8;
+            int numberOfColumns = 8;
+
+
+            foreach (char column in alphabet)
+            {
+                numberOfColumns++;
+                this.chessBoard.Columns.Add(column.ToString(), column.ToString());
+            }
+
+            this.chessBoard.Rows.Add(numberOfRows);
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                this.chessBoard.Rows[i].HeaderCell.Value = (i + 1).ToString();
+            }
+
+            for (int i = 0;i <numberOfColumns)
+
+            this.chessBoard.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            this.chessBoard.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
         }
     }
 }
