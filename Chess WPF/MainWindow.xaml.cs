718e1be7345fc80ca725b6d2d63d3f720d539454
cromeros <cromerosanchez@wsu.edu'clear>
2023-06-13 00:43:38 -0700

    l change to wpf rather than winforms as it is easier to create the front end of things. Created main menu window and chess board window.

---
+++ Chess WPF/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace Chess_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Human_Locally_Button_Click(object sender, RoutedEventArgs e)
+        {
+            ChessBoardWindow chessBoardWindow = new ChessBoardWindow();
+            chessBoardWindow.Show();
+            this.Close();
+        }
+    }
+}
